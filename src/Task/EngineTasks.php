a56e5cd93b3baf177b8b39437e777c1e91527225
Travis Tomka <underoathosx@gmail.com>
2017-03-22 09:13:56 -0600

    Add hostsfile management into the engine up/down commands.

--- src/Task/EngineTasks.php
+++ src/Task/EngineTasks.php
@@ -2,6 +2,9 @@
 
 namespace Droath\ProjectX\Task;
 
+use Droath\HostsFileManager\HostsFile;
+use Droath\HostsFileManager\HostsFileWriter;
+
 /**
  * Define Project-X engine task commands.
  */
@@ -9,13 +12,23 @@ class EngineTasks extends TaskBase
 {
     /**
      * Startup project engine.
+     *
+     * @option $no-hostname Don't add hostname to the system hosts file
+     *   regardless if it's defined in the project-x config.
+     * @option $no-browser Don't open the browser window on startup regardless
+     *   if it's defined in the project-x config.
      */
-    public function engineUp()
+    public function engineUp($opts = ['no-hostname' => false, 'no-browser' => false])
     {
         $this->engineInstance()->up();
 
         // Allow projects to react to the engine startup.
         $this->projectInstance()->onEngineUp();
+
+        // Add hostname to the system hosts file.
+        if (!$opts['no-hostname']) {
+            $this->addHostName($opts['no-browser']);
+        }
 
         return $this;
     }
@@ -29,6 +42,61 @@ class EngineTasks extends TaskBase
 
         // Allow projects to react to the engine shutdown.
         $this->projectInstance()->onEngineDown();
+
+        // Remove hostname from the system hosts file.
+        $this->removeHostName();
+
+        return $this;
+    }
+
+    /**
+     * Add hostname to hosts file.
+     *
+     * @param bool $no_browser
+     *   Don't open the browser window.
+     */
+    protected function addHostName($no_browser)
+    {
+        $config = $this->getProjectXConfig();
+
+        if (isset($config['host'])) {
+            $host = $config['host'];
+            $hostsfile = (new HostsFile())
+                ->setLine('127.0.0.1', $host['name']);
+
+            (new HostsFileWriter($hostsfile))->add();
+
+            $this->say(
+                sprintf('Added %s to hosts file.', $host['name'])
+            );
+
+            if (!$no_browser && $host['open_on_startup'] == 'true') {
+                $this->taskOpenBrowser('http://' . $host['name'])
+                    ->run();
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove hostname from hosts file.
+     */
+    protected function removeHostName()
+    {
+        $config = $this->getProjectXConfig();
+
+        if (isset($config['host'])) {
+            $host = $config['host'];
+            $hostsfile = (new HostsFile())
+                ->setLine('127.0.0.1', $host['name']);
+
+            (new HostsFileWriter($hostsfile))->remove();
+
+            $this->say(
+                sprintf('Removed %s from hosts file.', $host['name'])
+            );
+        }
 
         return $this;
     }
