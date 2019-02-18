85a7a4879bd041f4c5cb565356646e5f5e2435b7
Travis Tomka <underoathosx@gmail.com>
2019-02-18 12:24:14 -0700

    Update GitDeploy to verify the latest tag was using semantic versioning.

--- src/Deploy/GitDeploy.php
+++ src/Deploy/GitDeploy.php
@@ -173,7 +173,9 @@ class GitDeploy extends DeployBase
         $result = $this->runSilentCommand($task);
         $version = trim($result->getMessage());
 
-        return !empty($version) ? $version : '0.0.0';
+        return !empty($version) && preg_match('/(\d.\d.\d)/', $version)
+            ? $version
+            : '0.0.0';
     }
 
     /**
